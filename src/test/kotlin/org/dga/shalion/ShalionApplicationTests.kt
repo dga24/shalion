9f9964cafdfddc6b0fb57091706a6ba03ec1ef54
david <garciadavid.24@gmail.com>
2025-08-02 10:40:14 +0200

    1st

---
+++ src/test/kotlin/org/dga/shalion/ShalionApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.dga.shalion
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ShalionApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
